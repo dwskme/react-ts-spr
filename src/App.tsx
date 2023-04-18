874e75bf50d4661f525e36a49bc3bedee70f6c56
dwskme <officialsamirkc@gmail.com>
2023-04-19 00:40:36 +0545

    feat: Init new React-TS Scissors paper rock game.

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className={styles.container}>
+      <div className={styles.titleCtn}>
+        <h1>Scissors, Paper, Rock</h1>
+        <p>React TS Game.</p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
